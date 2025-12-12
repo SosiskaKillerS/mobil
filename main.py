c5ef860945f80117784c256d447ace8de52767e8
SosiskaKillerS <uka.blyatsuka@yandex.ru>
2025-12-12 18:06:10 +0300

    make startup database

--- main.py
+++ main.py
@@ -1,72 +1,23 @@
-from http.client import HTTPException
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from starlette.middleware.cors import CORSMiddleware
+from init_db import engine, Base, get_session
+from models import User
+from schemas import UserCreate, UserLogin
+app = FastAPI()
 
-from fastapi import FastAPI
-from typing import Literal
-from pydantic import BaseModel
-
-app = FastAPI(
-    title="FanMonet fake billing api",
-    description='Fake billing for our uni mobile application'
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
 )
 
-SubscriptionStatusLiteral = Literal['none', 'pending', 'active']
-
-class SubscriptionStatus(BaseModel):
-    status: SubscriptionStatusLiteral
-
-class CreatePaymentResponse(BaseModel):
-    payment_id: int
-    subscription_status: SubscriptionStatus
-
-class FakeWebhookBody(BaseModel):
-    payment_id: int
-
-subscription_store = {
-    'user1': SubscriptionStatus(status='none')
-}
-
-payments_store: dict[int, dict] = {}
-next_payment_id = 1
-
-def get_current_username()->str:
-    return 'user1'
-
-@app.get('/subscription', response_model=SubscriptionStatus)
-def get_subscription_status() -> SubscriptionStatus:
-    username = get_current_username()
-    return subscription_store[username]
-
-@app.post('/payments/create', response_model=CreatePaymentResponse)
-def create_payment() -> CreatePaymentResponse:
-    global next_payment_id
-
-    username = get_current_username()
-    sub = subscription_store[username]
-
-    if sub.status == 'active':
-        return CreatePaymentResponse(
-            payment_id=0,
-            subscription_status=sub.status,
-        )
-    payment_id = next_payment_id
-    next_payment_id += 1
-    payments_store[payment_id] ={
-        'id': payment_id,
-        'user': username,
-        'status': 'pending',
-    }
-    subscription_store[username] = SubscriptionStatus(status='pending')
-    return CreatePaymentResponse(
-        payment_id=payment_id,
-        subscription_status='pending',
-    )
-
-@app.post('/webhooks/fake_payment', response_model=SubscriptionStatus)
-def fake_payment_webhook(body: FakeWebhookBody) -> SubscriptionStatus:
-    payment = payments_store.get(body.payment_id)
-    if not payment:
-        raise HTTPException(status_code=404, detail = 'Payment not found')
-    payment['status']='success'
-    username = payment['user']
-    subscription_store[username] = SubscriptionStatus(status='active')
-    return subscription_store[username]
+@app.post("/sturtup")
+async def on_startup():
+    async with engine.begin() as connection:
+        await connection.run_sync(Base.metadata.drop_all)
+        await connection.run_sync(Base.metadata.create_all)
+    return {'response': 'success'}
