e1110b069f8241be45b5d74346d9c967b6f7bfa5
SosiskaKillerS <uka.blyatsuka@yandex.ru>
2026-01-15 23:07:07 +0300

    i forgot commit fucking schemas

--- schemas.py
+++ schemas.py
@@ -20,3 +20,17 @@ class UserRegistration(BaseModel):
 
 class Verify(BaseModel):
     code: str = Field(min_length=4, max_length=4)
+
+class RestorePassword(BaseModel):
+    email: EmailStr | None = None
+    username: str | None = Field(default=None, min_length=3, max_length=25)
+
+    @model_validator(mode="after")
+    def check_login(self):
+        if self.email is None and self.username is None:
+            raise ValueError("Provide email or username")
+        return self
+
+class RestorePasswordPatch(Verify):
+    new_password: str = Field(min_length=6, max_length=12)
+
